e8feaa26b826f59f3b95490ae8b369d0d7a75a7d
Łukasz Szumilas <lukaszszumilas9@wp.pl>
2020-08-06 08:59:50 +0200

    adding some learning stuff

---
+++ automateboringstuff/boring1.py
@@ -0,0 +1,5 @@
+print("Hello, world!")
+myName = input("What is your Name?")
+print("Your lengh of name is: ", len(myName))
+myAge = int(input("What is your age?"))
+print("You will be ",myAge + 1, " in a Year")
