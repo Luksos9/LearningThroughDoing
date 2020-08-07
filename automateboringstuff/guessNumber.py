5be3882bf80bd8eadab6069652d3bb4c9b997aa4
Łukasz Szumilas <68565480+Luksos9@users.noreply.github.com>
2020-08-07 14:06:02 +0200

    Update guessNumber.py
    Changing prints from mixed  Polish and English to English

--- automateboringstuff/guessNumber.py
+++ automateboringstuff/guessNumber.py
@@ -10,10 +10,10 @@ while randomNumber != guess and guesslimit < 6:
     try:
         guess = int(input("Take a guess"))
         guesslimit += 1
-        print("To twoja ",guessCounter, "/6")
+        print("It is yours ",guessCounter, "/6")
         guessCounter += 1
     except ValueError or TypeError or guess > 20 or guess < 0:
-        print("wprowadz liczbe od 1-20")
+        print("Enter a number between 1-20")
         continue
 
     if guess < randomNumber:
