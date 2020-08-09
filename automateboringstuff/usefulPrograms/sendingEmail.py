bb69984af365f8c61a8825014d806baf6a5c15da
Łukasz Szumilas <lukaszszumilas9@wp.pl>
2020-08-09 18:11:57 +0200

    Adding function instead of raw text

--- automateboringstuff/usefulPrograms/sendingEmail.py
+++ automateboringstuff/usefulPrograms/sendingEmail.py
@@ -2,8 +2,30 @@
 
 import smtplib
 
+def sendInfoEmail(user, password, mailFrom, mailTo, mailSubject, mailBody):
+
+    message = """From: {}
+Subject: {}
+    
+{}
+""".format(mailFrom, mailSubject, mailBody)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(user, password)
+        server.sendmail(user, mailTo, message.encode('utf8')) #including .encode is important for polish characters
+        server.close()
+        print("mail sent") #Confirming that it was successful
+        return True
+
+    except:
+
+        print('Error sending email') #Informs that there has been error
+        return False
+
 mailFrom = 'Your automation system' #From whom email is being send (it can be Your Name)
-mailTo = ['exampleMail@gmail.com'] #Adress that we are sending email to (it is list so can be several)
+mailTo = ['exampleEmail@gmail.com'] #Adress that we are sending email to (it is list so can be several)
 mailSubject = 'Processing finished successfully' #E-mail subject
 mailBody = '''Hello  
 
@@ -11,23 +33,7 @@ This mail confirms that processing has finished without problems,
 
 Have a nice day''' #Email content
 
-message = """From: {}
-Subject: {}
+user = 'example@gmail.com' #Enter your email here
+password = 'examplePassword' #Enter your password here
 
-{}
-""".format(mailFrom, mailSubject, mailBody)
-
-user = 'uzytkownik123@gmail.com' #Enter your email here
-password = "veryConcealedPassword" #Enter your password here
-
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(user, password)
-    server.sendmail(user, mailTo, message.encode('utf8')) #including .encode is important for polish characters
-    server.close()
-    print("mail sent") #Confirming that it was successful
-
-except:
-
-    print('Error sending email') #Informs that there has been error
+sendInfoEmail(user, password, mailFrom, mailTo, mailSubject, mailBody)
