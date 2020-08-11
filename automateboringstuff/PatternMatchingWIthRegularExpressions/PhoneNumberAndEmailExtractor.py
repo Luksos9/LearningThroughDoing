a0af32bbf6f48d4941f1256f9795b888bda9271b
Łukasz Szumilas <lukaszszumilas9@wp.pl>
2020-08-11 11:18:49 +0200

    Adding Email regex and few corrections/ comments

--- automateboringstuff/PatternMatchingWIthRegularExpressions/PhoneNumberAndEmailExtractor.py
+++ automateboringstuff/PatternMatchingWIthRegularExpressions/PhoneNumberAndEmailExtractor.py
@@ -4,16 +4,27 @@
 import pyperclip
 import re
 
-phoneRegex = re.compile(r"""(
+# Creat Phone Number regex
+
+phoneRegex = re.compile(r"""(   #r used to read 'raw' text, so as it is
 (\d{3}|\(\d{3}\))?              #the area of code
-(\s|-|\.)?                      #separator -
+(\s|-|\.)?                      #separator " " or - or .
 (\d{3})                         #first 3 digits
 (\s|-|\.)                       #separator
 (\d{4})                         #last 4 digit
-(\s*(ext|x|ext.)\s*(\d{2,5}))?  #extension
+(\s*(ext|x|ext.)\s*(\d{2,5}))?  #optional extension
 )""", re.VERBOSE)
 
 # TODO: Create email regex.
+
+# Create Email regex
+
+emailRegex = re.compile(r"""(
+[a-zA-Z0-9._%+-]+   #username
+@                   #@ symbol
+[a-zA-Z0-9.-]+      #name of domain
+(\.[a-zA-Z]{2,4})   #dot and after it something (.pl or .com etc)
+)""", re.VERBOSE)    #VERBOSE allows you to add whitespace and comments
 
 # TODO: Find matches in clipboard text.
 
