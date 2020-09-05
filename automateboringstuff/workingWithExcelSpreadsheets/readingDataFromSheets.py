fdb08bfae5fe2e75658a94280d97dbde7638ba3a
Łukasz Szumilas <lukaszszumilas9@wp.pl>
2020-09-05 18:47:49 +0200

    Added: 1)Make sure the key for this state exists. 2)Make sure the key for this county in this state exists. 3)Each row represents one census tract, so increment by one 4)Increase the county pop by the pop in this census tract.

--- automateboringstuff/workingWithExcelSpreadsheets/readingDataFromSheets.py
+++ automateboringstuff/workingWithExcelSpreadsheets/readingDataFromSheets.py
@@ -9,12 +9,21 @@ wb = openpyxl.load_workbook(r'C:\Users\lukas\OneDrive\Pulpit\automateboringStuff
 sheet = wb.active
 countyData = {}
 
-# TODO: Fill in countyData with each county's population and tracts.
 print('Reading rows...')
 for row in range(2, sheet.max_row + 1):
     # Each row in the spreadsheet has data for one census tract
     state = sheet['B' + str(row)].value
     county = sheet['C' + str(row)].value
     pop = sheet['D' + str(row)].value
+
+    # Make sure the key for this state exists.
+    countyData.setdefault(state, {})
+    # Make sure the key for this county in this state exists.
+    countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+
+    # Each row represents one census tract, so increment by one
+    countyData[state][county]['tracts'] += 1
+    # Increase the county pop by the pop in this census tract.
+    countyData[state][county]['pop'] += int(pop)
 
 # TODO: Open a new text file and write the contents of countyData to it
